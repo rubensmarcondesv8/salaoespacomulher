a92123204ba8be7cb0df1bfb6fb66719b7f40d87
rubensmarcondesv8 <rubensmarcondesv8@gmail.com>
2021-09-05 12:43:06 -0300

    Primeiro commit

---
+++ src/main/java/com/elizelia/salaoespacomulher/SalaoespacomulherApplication.java
@@ -0,0 +1,13 @@
+package com.elizelia.salaoespacomulher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoespacomulherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoespacomulherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elizelia/salaoespacomulher/SalaoespacomulherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elizelia.salaoespacomulher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoespacomulherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
